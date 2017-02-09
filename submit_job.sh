d36e0079de3490d2b4cf26b6b323c7e402a606d7
Metodos computacionales avanzados je.forero <j.e.forero.romero@gmail.com>
2017-02-08 20:51:52 -0500

    submit file bash

---
+++ submit_job.sh
@@ -0,0 +1,10 @@
+#PBS -q batch
+#PBS -N test_scheduler
+#PBS -l nodes=1:ppn=2
+#PBS -M mneira011@gmail.com
+#PBS -m abe
+
+module load openmpi/1.8.5
+cd $PBS_O_WORKDIR
+NPROCS=`wc -l < $PBS_NODEFILE`
+mpiexec -v -n $NPROCS ./placas.x
